1d0493b179623f1dff4b36dc1cd6731e2b2b5d41
Grazielly Lopes <grazilly-ltdsantos@educar,rs,gov,br>
2023-07-31 20:18:33 -0300

    terceiro

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express'); 
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(expressLayouts)
+app.use(expressLayouts);
+
+app.set('layout','./layouts/defaut/index')
 const tarefaController = require('./controllers/tarefaController'); 
+/*const loginRouter = require('./login');
+const indexRouter = require('./index');
+const usersRouter = require('./users');*/
 
-const app = express(); 
 const port = 5000; 
 
 app.set('view engine', 'ejs'); 
+app.use(express.static("public"));
+
+app.set('layout','./layouts/default/index');
 
 app.use(express.urlencoded({ extended: true })); 
 
@@ -18,6 +29,10 @@ app.delete('/tarefa',tarefaController.deleteTarefa);
 // app.put('/tarefa',tarefaController.updateTarefa);
 // app.get('/tarefa/edit',tarefaController.editTarefa);
 // app.get('/tarefa',tarefaController.searchTarefa);
+
+//app.use('/login',loginRouter);
+//app.use('/users',usersRouter);
+//app.use('/index',indexRouter);
 
 app.listen(port, () => { 
 console.log(`Servidor rodando em http://localhost:${port}`);
